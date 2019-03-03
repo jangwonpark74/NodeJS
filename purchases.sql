b467f470b93285c5908e853e42691909517f65cf
jang won park <jangwonpark74@gmail.com>
2019-03-03 17:49:06 +0900

    Create purchases.sql

---
+++ purchases.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXIST `purchases` (
+ `id` int(11) NOT NULL AUTO_INCREAMENT,
+ `userid` int NOT NULL,
+ `goodsid` int NOT NULL,
+ `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ PRIMARY KEY(`id`)
+ ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
